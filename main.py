02536344e1390a2fbcd6bf3787f221c486023f1a
Front-end developer <anna.oblu@gmail.com>
2019-12-26 23:50:13 +0500

    test 1

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+adjectives = ['cubic', 'fragrant', 'rough', 'wet', 'calm', 'hairy',
+              'sleepy', 'slow', 'hot', 'cold', 'big', 'red', 'orange',
+              'yellow', 'green', 'blue']
+nouns = ['apple', 'dinosaur', 'ball', 'goat', 'dragon', 'car', 'car',
+         'eggplant ', 'jelly', 'sugar', 'avocado']
+while True:
+    for num in range(3):
+        print('Добро пожаловать')
+        adjective = random.choice(adjectives)
+        noun = random.choice(nouns)
+        number = random.randrange(1, 100)
+        special_char = random.choice(string.punctuation)
+        password = adjective + noun + str(number) + special_char
+        print('Новый пароль: ' + password)
+    response = input('Сгенирировать новый пароль? Введите д или н')
+    if response == 'н':
+        break
+
+
+
+
+
